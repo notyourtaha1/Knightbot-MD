c695f87c4e794a3979ce6e455787dcd09fc4c9eb
notyourtaha1 <tahahere2010@gmail.com>
2025-05-09 18:08:53 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Mahiru Bot",
-  botOwner: '🗣️ T A H A 🔥', // Your name
+  botOwner: 'T A H A', // Your name
   ownerNumber: '923004204338', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
